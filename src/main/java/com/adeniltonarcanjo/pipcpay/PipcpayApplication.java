eff347691946d5564ad1443041f50e209e56680b
adeniltonarcanjo <adeniltonmorais@gmail.com>
2023-09-02 05:33:11 -0300

    start project

---
+++ src/main/java/com/adeniltonarcanjo/pipcpay/PipcpayApplication.java
@@ -0,0 +1,13 @@
+package com.adeniltonarcanjo.pipcpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipcpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipcpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adeniltonarcanjo/pipcpay/PipcpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adeniltonarcanjo.pipcpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipcpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
